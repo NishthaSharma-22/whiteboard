5ac3979b499bb7b7a7124c11c5b23af62a0c1f32
Nishtha Sharma <nishthapinjore@gmail.com>
2025-09-19 17:36:04 +0530

    fixed blank board page layout and button

--- app/(dashboard)/_components/blank-board/blank-board-page.tsx
+++ app/(dashboard)/_components/blank-board/blank-board-page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { useMutation } from "convex/react";
 import { api } from "@/convex/_generated/api";
 import { useOrganization } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
 
 export default function BlankBoardPage() {
   const create = useMutation(api.board.create);
@@ -12,16 +13,16 @@ export default function BlankBoardPage() {
     create({ orgId: organization.id, title: "unititle" });
   };
   return (
-    <div className="flex flex-col justify-center items-center h-full">
+    <div className="flex flex-col justify-center items-center h-full gap-y-3">
       <Image
         src="/globe.svg"
         alt="favs not found image"
         width={200}
         height={200}
       />
-      <h3 className="m-[20]">No favorite boards yet.</h3>
+      <h3>No boards yet.</h3>
       <p>Create your first board to get started!</p>
-      <button onClick={onClick}>Create</button>
+      <Button onClick={onClick} className="bg-emerald-400 hover:bg-emerald-600 cursor-pointer p-[20px] font-semibold">Create Board</Button>
     </div>
   );
 }
